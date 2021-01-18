5600f4b8e17fdcada121d42f0368512a22151196
Jordan LCQ <jordan.lcq1@gmail.com>
2021-01-18 18:43:32 +0100

    first commit

--- backend/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import * as AuthController from "./src/authController"
-import * as ItemController from './src/itemController'
-
-export {
-    AuthController,
-    ItemController
-}
--- backend/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import * as authRoutes from "./src/authRoutes";
-import * as itemRoutes from "./src/itemRoutes";
-
-export {
-    authRoutes,
-    itemRoutes
-}
